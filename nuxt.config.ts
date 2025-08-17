578ee3b0f501563a8e2590d2c2ea2efb5cb135b8
GwynDev <root.gwyn@hotmail.com>
2025-08-17 13:59:28 +0300

    chore: added config for env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss'],
+
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.API_BASE_URL,
+    },
+  },
 });
